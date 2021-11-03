65795843cc39fa00b07b6dc0df570bad846ef49a
wanghao <wanghao4999@163.com>
2021-11-03 07:41:03 +0000

    add redis/autoJoin.sh.

---
+++ redis/autoJoin.sh
@@ -0,0 +1,31 @@
+echo "开始自动加入集群"
+
+dockerId=$(docker ps -a | grep redis | awk '{print $1}'|head -1)
+
+echo $dockerId
+
+docker exec -i $dockerId bash <<EOF
+
+pwd
+whoami
+last root
+
+# echo "dir:$(pwd)"
+
+echo "查看redis 集群信息"
+
+redis-cli -h 1.116.17.54 -p 6000
+
+cluster nodes
+
+cluster info
+
+echo "执行redis命令"
+
+set 234 234
+get 234
+
+EOF
+
+echo "退出Docker容器($dockerId)"
+
