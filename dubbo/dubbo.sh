993cfafac5ce3828e35473e995f35467ecfd89a7
wanghao229 <229332570@qq.com>
2021-11-12 10:50:29 +0800

    Update dubbo.sh

--- dubbo/dubbo.sh
+++ dubbo/dubbo.sh
@@ -1,1 +1,31 @@
+#/bin/bash
 
+hostname=$(hostname)
+echo "hostname:${hostname}"
+
+echo "hostname:"${hostname//VM/123}
+
+echo  ${hostname//-/\\\-}
+
+echo "请输入外网ip地址"
+read ip
+echo "ip:${ip}"
+
+echo "------------------------------"
+
+dest="/root/hosts"
+
+echo $dest
+echo "s/^[^#].*$hostname.*/#&/"
+
+sed -i "s/^[^#].*$hostname.*/#&/" $dest
+sed -i "s/^#\($ip.*\)/\1/" hosts
+
+#grep -c "$ip" "$dest"
+
+if [ `grep -c "$ip" "$dest"` -eq '0' ];then
+  echo "配置Dubbo外网地址:$dest"
+  echo "$ip $hostname" >> $dest
+else
+  echo "已配置"
+fi
