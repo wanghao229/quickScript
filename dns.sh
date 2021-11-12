be3fe78930de5f9d764b4af26df487c09f7a6f90
wanghao229 <229332570@qq.com>
2021-11-12 11:22:45 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -9,9 +9,10 @@ cp $dest{,.backup."$(date +%Y%m%d-%H%M%S)"}
 sed -i "/^[^#].*eureka-server.*/d" $dest
 
 echo "配置dns"
-
-
 cat >>$dest<<EOF
 101.34.168.240 eureka-server1
 101.34.168.240 eureka-server2
 EOF
+
+echo "---------------------------"
+cat $dest
