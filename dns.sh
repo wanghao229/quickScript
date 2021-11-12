7fab9f98318f22a69d99a86ca806a7a8ac020711
wanghao229 <229332570@qq.com>
2021-11-12 11:19:30 +0800

    Create dns.sh

---
+++ dns.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+dest="/etc/hosts"
+
+echo $dest
+
+cp $dest{,.backup."$(date +%Y%m%d-%H%M%S)"}
+
+sed -i "/^[^#].*eureka-server.*/d" $dest
+
+echo "配置dns"
+
+
+cat >>$dest<<EOF
+101.34.168.240 eureka-server1
+101.34.168.240 eureka-server2
+EOF
