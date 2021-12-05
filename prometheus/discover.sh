6caaf71e17d60535ac3634e254f5a7a994317663
wanghao229 <229332570@qq.com>
2021-12-05 17:29:27 +0800

    Update discover.sh

--- prometheus/discover.sh
+++ prometheus/discover.sh
@@ -2,7 +2,7 @@
 jvmJson="/opt/prometheus/targets-jvm.json"
 
 echo "更新$jvmJson"
-curl -s -H 'Accept:application/json' http://eureka-server1:8761/eureka/apps | jq '[.applications.application[] | select(.name != "GATEWAY") | { labels:{job:.name,instance:.instance[] | .instanceId},targets:[ .instance[] | .ipAddr + ":" + (.port["$"] + 10000 | tostring) ] }]' > $jvmJson
+curl -s -H 'Accept:application/json' http://eureka-server1:8761/eureka/apps | jq '[.applications.application[] | select(.name != "GATEWAY") | { labels:{job:"java",instance:.instance[] | .instanceId},targets:[ .instance[] | .ipAddr + ":" + (.port["$"] + 10000 | tostring) ] }]' > $jvmJson
 
 
 echo "------------------------------------"
