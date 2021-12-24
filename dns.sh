2a8e07cbf29520470b058be4fe01a05ffa28e6aa
wanghao229 <229332570@qq.com>
2021-12-24 09:23:24 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -15,6 +15,7 @@ sed -i "/^[^#].*dbhost.*/d" $dest
 sed -i "/^[^#].*extIp.*/d" $dest
 sed -i "/^[^#].*oauth2Host.*/d" $dest
 sed -i "/^[^#].*github.global.ssl.Fastly.net.*/d" $dest
+sed -i "/^[^#].*es.*/d" $dest
 
 echo "删除 $profile 旧配置文件"
 sed -i "/^[^#].*extIp.*/d" $profile
@@ -26,6 +27,8 @@ cat >>$dest<<EOF
 101.34.168.240 dbhost
 101.34.248.75 oauth2Host
 199.232.69.194 github.global.ssl.Fastly.net
+150.158.52.34 es7Host
+101.34.248.75 es6Host
 EOF
 
 echo "---------------------------"
